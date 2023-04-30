2416d19331162d1d0ab8777f6d2eb13f3a3783b3
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-04-30 12:36:09 +0400

    add: Integration test

---
+++ src/test/java/ru/example/vacationpaycalculator/integration/annotation/IT.java
@@ -0,0 +1,18 @@
+package ru.example.vacationpaycalculator.integration.annotation;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+@SpringBootTest
+public @interface IT {
+}
